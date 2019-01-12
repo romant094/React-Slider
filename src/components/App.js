75eb49b29710255ad892604ffa76d943420f1a1e
Anton Romankov <romant094@gmail.com>
2019-01-12 16:03:09 +0300

    PSD to HTML

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,56 @@
 import React from 'react';
 
+import './app.css';
+
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './header';
+import Main from './main';
+import Menu from './menu';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 48px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 function App() {
     return (
-        <div>
-            <Hello name='Anton' surname='Romankov' link='http://vk.com' />
+        <div className="app">
+            <HeaderWrapper>
+                <Grid>
+                    <Header />
+                </Grid>
+            </HeaderWrapper>
+
+            <MenuWrapper>
+                <Grid>
+                    <Menu />
+                </Grid>
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main />
+                </Grid>
+            </MainWrapper>
         </div>
     );
-}
-
-function Hello({ name, surname, link }) {
-    return (
-        <div>
-            <div>
-                <h1>{name} {surname}!</h1>
-                <a href={link}>VK profile</a>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi provident impedit nulla excepturi rerum, nam numquam ex quis id error?</p>
-            </div>
-        </div>
-    )
 }
 
 export default App;
